7c31792c675e065010627f615c13ec69e1a14179
Hector Nevado <hectornevado46@gmail.com>
2024-12-25 21:54:42 +0100

    Creada la estructura de las funciones principales del plugin

--- seo-images-alt-generator.php
+++ seo-images-alt-generator.php
@@ -6,5 +6,31 @@ Version: 1.0
 Author: Héctor Nevado (@hnevado.dev)
 */
 
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+// Agregar el menú en el administrador
+add_action('admin_menu', function () {
+    add_menu_page(
+        'SEO Images',
+        'SEO Images',
+        'manage_options',
+        'seo-images',
+        'seo_images_page',
+        'dashicons-format-image',
+        6
+    );
+});
+
+// Función para mostrar el listado de imágenes sin ALT
+function seo_images_page() {
+ 
+}
+
+// Función para generar los ALT
+function seo_generate_alt($images) {
+ 
+}
 
 ?>
