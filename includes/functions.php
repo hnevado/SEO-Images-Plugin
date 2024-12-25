2e29ed583c8a5a01c8ed918aabc58e4d03855935
Hector Nevado <hectornevado46@gmail.com>
2024-12-26 00:01:53 +0100

    Eliminado código antiguo y añadido comentarios en código

--- includes/functions.php
+++ includes/functions.php
@@ -4,8 +4,8 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * Genera atributos ALT para imágenes sin ALT.
- *
+ * Función responsable de generar el atributo alt para las imágenes 
+ * que no tienen este atributo en las páginas y entradas de WordPress.
  * @param array $images
  */
 
@@ -13,10 +13,14 @@ function seo_generate_alt($images) {
     $updated_count = 0;
 
     foreach ($images as $image) {
-        $post_id = $image['post_id'];
-        $post_content = get_post_field('post_content', $post_id);
-        $post_title = get_the_title($post_id);
-        $image_src = $image['image_src'];
+
+        $post_id = $image['post_id']; //ID del post (entrada o página)
+        $post_content = get_post_field('post_content', $post_id); //Contenido del post
+        $post_title = get_the_title($post_id); //El título del post, que se utilizará como valor para el atributo alt
+        $image_src = $image['image_src']; //La URL de la imagen
+
+        //preg_replace_callback —> Realiza una búsqueda y sustitución de una expresión regular usando una llamada de retorno
+        //https://www.php.net/manual/es/function.preg-replace-callback.php
 
         $updated_content = preg_replace_callback(
             '/(<img[^>]*src=["\']' . preg_quote($image_src, '/') . '["\'][^>]*?)alt=["\']?[^"\']*["\']?([^>]*>)/i',
@@ -26,6 +30,7 @@ function seo_generate_alt($images) {
             $post_content
         );
 
+        //Si el contenido actualizado es diferente al contenido original (el atributo alt se ha agregado), actualizamos el post
         if ($updated_content !== $post_content) {
             wp_update_post([
                 'ID' => $post_id,
