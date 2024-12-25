124245d607971fc8f65c3f66f394f1310f336b36
Hector Nevado <hectornevado46@gmail.com>
2024-12-25 21:40:13 +0100

    Creación del fichero principal del plugin con el nombre y descripción

---
+++ seo-images-alt-generator.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: SEO Images ALT Generator
+Description: Añade automáticamente atributos ALT a imágenes que no lo tienen, basados en el nombre de la página o entrada.
+Version: 1.0
+Author: Héctor Nevado (@hnevado.dev)
+*/
+
+
+?>
